91034821d34da348bdd25dd80581a2ec3922a4b0
robertocannella <robertocannella@gmail.com>
2022-08-29 14:07:59 -0400

    Add comment to The SELECT Clause

--- 2-the_select_clause.sql
+++ 2-the_select_clause.sql
@@ -1,8 +1,9 @@
-SELECT 
+-- Selects the listed columns, creates a new discount_factor column based on calculations of the points coloumn 
+SELECT
 	last_name, 
     first_name,
     points, 
-    (points + 10) *  100 AS "discountfactor"
+    (points + 10) *  100 AS "discount_factor"
 FROM customers 
 
 
