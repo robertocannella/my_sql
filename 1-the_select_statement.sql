c09ca80ae8847d079521e86d15745d01d27acbfd
robertocannella <robertocannella@gmail.com>
2022-08-29 14:16:49 -0400

    Delete duplicates

--- 1-the_select_statement.sql
+++
@@ -1,6 +0,0 @@
-USE sql_store;
-SELECT * 
-FROM customers 
-WHERE customer_id = 1
-ORDER BY first_name
-
--- 2-the_select_clause.sql
+++
@@ -1,9 +0,0 @@
--- Selects the listed columns, creates a new discount_factor column based on calculations of the points coloumn 
-SELECT
-	last_name, 
-    first_name,
-    points, 
-    (points + 10) *  100 AS "discount_factor"
-FROM customers 
-
-
